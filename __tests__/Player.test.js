624a215a6c1b0b363b8efab8489a1f015091c0f8
utilrr <gmarkghealy@gmail.com>
2022-04-27 17:35:16 -0400

    Attack & potion methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -56,7 +56,6 @@ test("checks if player is alive or not", () => {
   expect(player.isAlive()).toBeFalsy();
 });
 
-/*
 test("subtracts from player's health", () => {
   const player = new Player("Dave");
   const oldHealth = player.health;
@@ -69,12 +68,30 @@ test("subtracts from player's health", () => {
 
   expect(player.health).toBe(0);
 });
-*/
 
-Player.prototype.reduceHealth = function (health) {
-  this.health -= health;
+test("gets player's attack value", () => {
+  const player = new Player("Dave");
+  player.strength = 10;
 
-  if (this.health < 0) {
-    this.health = 0;
-  }
-};
+  expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+  expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test("adds a potion to the inventory", () => {
+  const player = new Player("Dave");
+  const oldCount = player.inventory.length;
+
+  player.addPotion(new Potion());
+
+  expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test("uses a potion from inventory", () => {
+  const player = new Player("Dave");
+  player.inventory = [new Potion(), new Potion(), new Potion()];
+  const oldCount = player.inventory.length;
+
+  player.usePotion(1);
+
+  expect(player.inventory.length).toBeLessThan(oldCount);
+});
